1da38a5e5c08bd8d7fa7f073a3163a411bf7a5fc
Leeruigan <leeruigan@LeeruigandeMacBook-Pro.local>
2018-11-17 00:43:18 +0800

    异步async

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,24 @@ router.get('/string', async (ctx, next) => {
 })
 
 router.get('/json', async (ctx, next) => {
+  const a = await new Promise((resolve, reject) => {
+    setTimeout(function () {
+      console.log(new Date().getTime(), 'aaa')
+      resolve('aa') // 返回的值
+    }, 1000)
+  })
+  const c = await 123
+  const b = await new Promise((resolve, reject) => {
+    setTimeout(function () {
+      console.log(new Date().getTime(), 'bbb')
+      resolve('bb') // 返回的值
+    }, 1000)
+  })
   ctx.body = {
-    title: 'koa2 json'
+    title: 'koa2 json',
+    a,
+    b,
+    c
   }
 })
 
