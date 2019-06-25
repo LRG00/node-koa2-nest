87db814778057df7c23d6478d521a6ae75c50c42
LRG00 <158097628@qq.com>
2019-06-25 17:03:33 +0800

    dev

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,7 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  ctx.cookies.set('ccc', Math.random())
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string' // 页面返回字符串
-})
-
-router.get('/json', async (ctx, next) => {
-  const a = await new Promise((resolve, reject) => {
-    setTimeout(function () {
-      console.log(new Date().getTime(), 'aaa')
-      resolve('aa') // 返回的值
-    }, 1000)
-  })
-  const c = await 123
-  const b = await new Promise((resolve, reject) => {
-    setTimeout(function () {
-      console.log(new Date().getTime(), 'bbb')
-      resolve('bb') // 返回的值
-    }, 1000)
-  })
-  ctx.body = {
-    cookie: ctx.cookies.get('ccc'),
-    title: 'koa2 json',
-    a,
-    b,
-    c  // 页面返回json对象
-  }
+  ctx.redirect('http://149.28.161.252:10000')
 })
 
 module.exports = router
