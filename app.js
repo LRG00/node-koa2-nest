510890948875dbe4c77b7ad8bce6353f69ee5e09
LRG00 <158097628@qq.com>
2019-06-26 09:36:54 +0800

    dev

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ onerror(app)
 // 具体参数我们在后面进行解释
 app.use(cors({
   origin: function (ctx) {
-    return "*";
+    // return "*";
       // if (ctx.url === '/test') {
       //     return "*"; // 允许来自所有域名请求
       // }
-      // return 'http://localhost:8080'; // 这样就能只允许 http://localhost:8080 这个域名的请求了
+      return 'http://localhost:8080'; // 这样就能只允许 http://localhost:8080 这个域名的请求了
   },
   exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
   maxAge: 5,
