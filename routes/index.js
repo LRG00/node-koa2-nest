b5edf8e4ad39720f1e8b032bdec621815be230ae
LRG00 <158097628@qq.com>
2019-09-02 14:59:37 +0800

    访问首页的问题

--- routes/index.js
+++ routes/index.js
@@ -2,49 +2,11 @@ const router = require('koa-router')()
 const articleModel = require('../models/articleModel')
 
 router.get('/', async (ctx, next) => {
-  // const xxx = await articleController.list()
-  
-  const xxx =await articleModel.getArticleList()
+  let articleList = await articleModel.getArticleList({limit: 10, pageNo: 1})
   await ctx.render('index', {
-    title: 'Hello Koa 2!',
-    articleList: xxx
-  })
-})
-router.get('/index.html', async (ctx, next) => {
-  
-  const articleList =await articleModel.getArticleList()
-  await ctx.render('index', {
-    title: 'Hello Koa 2!',
     articleList
   })
-}) 
-router.get('/whisper.html', async (ctx, next) => {
-  await ctx.render('whisper')
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string' // 页面返回字符串
-})
-
-router.get('/json', async (ctx, next) => {
-  const a = await new Promise((resolve, reject) => {
-    setTimeout(function () {
-      resolve('aa') // 返回的值
-    }, 1000)
-  })
-  const c = await 123
-  const b = await new Promise((resolve, reject) => {
-    setTimeout(function () {
-      resolve('bb') // 返回的值
-    }, 1000)
-  })
-  ctx.body = {
-    cookie: ctx.cookies.get('ccc'),
-    title: 'koa2 json',
-    a,
-    b,
-    c  // 页面返回json对象
-  }
-})
 
 module.exports = router
