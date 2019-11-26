9deb56de2b4e2cfeb4ae38b99b8f12008da40cef
LRG00 <158097628@qq.com>
2019-11-26 08:31:19 +0800

    删除没用的文件

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # koa2-learn-demo
-npm install -g koa-generatoy
-koa2 -e koa2-learn-demo
-npm install --update-binary
+koa2-微信公众号api
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,9 @@ const router = require('koa-router')()
 const articleModel = require('../models/articleModel')
 
 router.get('/', async (ctx, next) => {
-  let articleList = await articleModel.getArticleList({ pageSize: 10, page: 1 })
-  await ctx.render('index', {
-    articleList
-  })
+  ctx.body = {
+    xx: 'hello world'
+  }
 })
 
 
