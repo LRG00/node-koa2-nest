70f9ea92747e215d1e81dbc55525229ccd133be9
LRG00 <158097628@qq.com>
2019-11-26 13:30:34 +0800

    babel

--- utils/db-util.js
+++ utils/db-util.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql")
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+import Sequelize from 'sequelize'
 var config = {
   database: 'abc', // 使用哪个数据库
   username: 'root', // 用户名
@@ -17,32 +18,3 @@ module.exports = new Sequelize(config.database, config.username, config.password
         idle: 30000
     }
 });
-
-// var Pet = sequelize.define('pet', {
-//   id: {
-//       type: Sequelize.STRING(50),
-//       primaryKey: true
-//   },
-//   name: Sequelize.STRING(100),
-//   gender: Sequelize.BOOLEAN,
-//   birth: Sequelize.STRING(10),
-//   createdAt: Sequelize.BIGINT,
-//   updatedAt: Sequelize.BIGINT,
-//   version: Sequelize.BIGINT
-// }, {
-//       timestamps: false
-//   });
-//   var now = Date.now();
-
-//   (async () => {
-//     var dog = await Pet.create({
-//         id: 'd-' + now,
-//         name: 'Odie',
-//         gender: false,
-//         birth: '2008-08-08',
-//         createdAt: now,
-//         updatedAt: now,
-//         version: 0
-//     });
-//     console.log('created: ' + JSON.stringify(dog));
-// })();
